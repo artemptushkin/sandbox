283dacab1a68e872ed3d20c6e13586b7106995df
artemptushkin <artem.ptushkin@ev-box.com>
2021-06-29 13:12:42 +0200

    Proper wrapper

--- src/main/java/ru/example/interf/service/CardServiceWrapper.java
+++ src/main/java/ru/example/interf/service/CardServiceWrapper.java
@@ -1,7 +1,14 @@
 package ru.example.interf.service;
 
-public class CardServiceWrapper extends DefaultCardService {
-	public CardServiceWrapper(CardStorageService cardStorageService) {
-		super(cardStorageService);
+import lombok.RequiredArgsConstructor;
+import ru.example.interf.domain.Card;
+
+@RequiredArgsConstructor
+public class CardServiceWrapper implements CardService {
+	private final CardService wrapped;
+
+	@Override
+	public Card getCard(Long cardId) {
+		return wrapped.getCard(cardId);
 	}
 }
